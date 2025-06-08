3eb78ec61664da8f2cb9fff3a3025e8d23a5983a
Ivan Mykhavko <tegosiv@gmail.com>
2025-06-08 14:50:42 +0000

    chore: code style

--- app/Actions/Catalog/ListCategoryTreeAdjacencyAction.php
+++ app/Actions/Catalog/ListCategoryTreeAdjacencyAction.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Actions\Catalog;
 
 use App\Actions\Actionable;
--- tests/Feature/Http/Api/V1/CategoryControllerTest.php
+++ tests/Feature/Http/Api/V1/CategoryControllerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature\Http\Api\V1;
 
 use App\Http\Resources\CategoryResource;
